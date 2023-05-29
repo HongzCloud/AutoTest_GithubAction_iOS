0d002b12b9e5349fe1ed09e2dee3b89b8fe8a6e9
HongzCloud <jihong21511@gmail.com>
2023-05-29 22:47:54 +0900

    Initial Commit

---
+++ AutoTest_GithubAction_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoTest_GithubAction_iOS
+//
+//  Created by 오킹 on 2023/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
